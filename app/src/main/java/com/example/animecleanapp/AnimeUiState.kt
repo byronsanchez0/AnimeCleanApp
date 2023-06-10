ac9fd6ff0adc24d777f5c52770d61e71f056e24b
byronsanchez0 <byroncarrillo52@yahoo.es>
2023-06-10 17:02:08 -0600

    anime ui state added

--- app/src/main/java/com/example/animecleanapp/AnimeUiState.kt
+++ app/src/main/java/com/example/animecleanapp/AnimeUiState.kt
@@ -1,5 +1,12 @@
 package com.example.animecleanapp
 
-//data class AnimeUiState(
-//    val
-//)
+import com.example.domain.model.AnimeModel
+import kotlinx.coroutines.flow.Flow
+
+data class AnimeUiState(
+    val animes: List<AnimeModel> = emptyList(),
+    val isLoading: Boolean = false,
+    val searchNews: (String) -> Unit,
+//    val saveSelectedFilter: (Filter) -> Unit,
+//    val selectedFilter: Flow<Filter>
+)
