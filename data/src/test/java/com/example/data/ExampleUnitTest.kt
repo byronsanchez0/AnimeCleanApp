a993ec25b1bd06f88aa61c237202c273251e5a51
byronsanchez0 <byroncarrillo52@yahoo.es>
2023-06-25 23:56:27 -0600

    testing DetailsViewModel and CharacterDetailsViewModel

--- data/src/test/java/com/example/data/ExampleUnitTest.kt
+++ data/src/test/java/com/example/data/ExampleUnitTest.kt
@@ -1,21 +1,21 @@
-package com.example.data
-
-import io.mockk.mockk
-import org.junit.Test
-
-import org.junit.Assert.*
-
-/**
- * Example local unit test, which will execute on the development machine (host).
- *
- * See [testing documentation](http://d.android.com/tools/testing).
- */
-class ExampleUnitTest {
-    @Test
-    fun searchAnimeText() {
-//        val searchSample = mockk<>()
-
-
-        assertEquals(4, 2 + 2)
-    }
-}
+//package com.example.data
+//
+//import io.mockk.mockk
+//import org.junit.Test
+//
+//import org.junit.Assert.*
+//
+///**
+// * Example local unit test, which will execute on the development machine (host).
+// *
+// * See [testing documentation](http://d.android.com/tools/testing).
+// */
+//class ExampleUnitTest {
+//    @Test
+//    fun searchAnimeText() {
+////        val searchSample = mockk<>()
+//
+//
+//        assertEquals(4, 2 + 2)
+//    }
+//}
